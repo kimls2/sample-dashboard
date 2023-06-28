a60e66b9ac93153867a6adc1f4b5c24aa50796cd
yisuk kim <yisuk@gmail.com>
2023-06-28 12:14:11 +0100

    use testing account

--- src/config/api-keys.js
+++ src/config/api-keys.js
@@ -1,10 +1,10 @@
 export const CONFIGURATION = {
   binance: {
-    key: 'mGRhcKoujXZdOy8OaIy1Qs4f4fZ1Oz0XlUJ8s2IPZ4nclQtBWmNaM4dhlBTgtIfy',
-    secret: 'VLUz4Ijnf8cbyN7pTzSshj30XClFifUfcgVL2bxINYCAGD1cVYr5s0XHH9CMqwO3',
+    key: '363fsUQSsP3qGKdFybLQnR1WXBRLPHxNOmVYJKzRGZm7ClQAZddWHRTFngEQtsxL',
+    secret: '88ToyghR98AvZCtyVkTuNPDOCDTHyG9WQWeYc4fExyc5hCKoXNH9G5HU7gxmAQzs',
     exchangeType: 'futures' // spot is not supported yet
   },
   profile: {
-    name: 'John Doe'
+    name: 'testing account 1k'
   }
 };
